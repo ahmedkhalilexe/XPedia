f4352de7bbc2b2e2686c806c2f2c1f6e605ddd1a
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-09-04 03:26:45 +0100

    feat: added cookie parser

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes/api"))
+app.use("/api", require("./routes/api"));
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
 });
 
 app.use(require("./middlewares/errorHandler"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
