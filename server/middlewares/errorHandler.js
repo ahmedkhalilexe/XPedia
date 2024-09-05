299c5ab878c0a63698bf632c1aa3d03e2f51ec11
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-09-05 20:43:15 +0200

    feat: handling jwt errors

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,18 +1,26 @@
 const AppError = require("../utils/AppError");
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 const errorHandler = (error, req, res, next) => {
-    console.error("Error caught in error handler");
-    console.error(error.message);
-    if(error instanceof AppError){
-        return res.status(error.statusCode).json({
-            status: error.statusCode,
-            message: error.message
-        });
-    }
-    return res.status(500).json({
-        status: 500,
-        message: "Something went wrong!"
+  console.error("Error caught in error handler");
+  console.error(error.message);
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: error.statusCode,
+      message: error.message,
     });
+  }
+  if (error instanceof JsonWebTokenError) {
+    res.clearCookie("rt");
+    return res.status(401).json({
+      status: 401,
+      message: "Invalid token",
+    });
+  }
+  return res.status(500).json({
+    status: 500,
+    message: "Something went wrong!",
+  });
 };
 
 module.exports = errorHandler;
