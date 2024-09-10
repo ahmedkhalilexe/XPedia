34d06faaf349f92acc59fce006ff3a0437d252a7
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-09-10 12:33:20 +0200

    feat: handle prisma errors

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,17 +1,28 @@
 const AppError = require("../utils/AppError");
 const { JsonWebTokenError } = require("jsonwebtoken");
-
+const {
+  PrismaClientKnownRequestError,
+} = require("@prisma/client/runtime/library");
 const errorHandler = (error, req, res, next) => {
   console.error("Error caught in error handler");
-  console.error(error.message);
   if (error instanceof AppError) {
+    //handle AppError
     return res.status(error.statusCode).json({
       status: error.statusCode,
       message: error.message,
     });
   }
+  if (error instanceof PrismaClientKnownRequestError) {
+    //handle Prisma errors
+    console.error("Error caught in Prisma error handler");
+    return res.status(401).json({
+      status: 401,
+      message: error.meta.cause,
+    });
+  }
   if (error instanceof JsonWebTokenError) {
-    console.log("Error caught in JWT error handler");
+    //handle JWT errors
+    console.error("Error caught in JWT error handler");
     res.clearCookie("rt");
     return res.status(401).json({
       status: 401,
