45eee9af9a7d486a3780f0c7dee2918790352245
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-09-11 19:27:22 +0200

    feat: /docs route for swagger

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const PORT = 3000;
 require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cookieParser());
 app.use(cors());
