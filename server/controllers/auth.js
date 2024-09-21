ede88f34f3bc41f00c3c338da2b0a4f105d6d61f
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-09-21 12:26:55 +0100

    refactor: keep just 401 error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -32,7 +32,7 @@ const authController = {
       },
     });
     if (!user) {
-      throw new AppError("User not found", 404);
+      throw new AppError("Invalid credentials", 401);
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
